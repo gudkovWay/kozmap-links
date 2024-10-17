ecc3b3e007b9b948f87d1c328638f11fbedf6a54
gudkovWay <gudkoviurii@gmail.com>
2024-10-17 22:15:32 +0300

    gdfgfd

--- src/components/LinkTabs/LinkTabs.tsx
+++ src/components/LinkTabs/LinkTabs.tsx
@@ -1,57 +1,39 @@
-'use client';
-import { AnimatePresence, motion } from 'framer-motion';
 import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
 
-import { kozmapLinks, oracleLinks } from './tabs';
+import { kozmapLinks } from './tabs';
 
 export const LinkTabs = () => {
-  const searchParams = useSearchParams();
-  const activeTab = searchParams.get('tab');
-
-  const currentTab = activeTab === 'kozmapLinks' || activeTab === null ? 'kozmapLinks' : 'oracleLinks';
-
-  const tabs = { kozmapLinks, oracleLinks };
-
   return (
     <div
       className='flex flex-col w-full h-full border-black rounded-md shadow-lg shadow-neutral-900 bg-neutral-800 pb-4'
     >
       <div className='flex flex-row h-full w-full gap-2 p-2 md:p-4'>
         <Link
-          href='/?tab=kozmapLinks'
-          className={`w-full text-center underline-offset-4 underline transition-all delay-100 ${currentTab === 'kozmapLinks' ? 'decoration-neutral-50 font-bold' : 'decoration-neutral-800 font-medium hover:decoration-neutral-200'}`}
+          href='/'
+          className='w-full text-center underline-offset-4 underline transition-all delay-100 decoration-neutral-50 font-bold}'
         >
           Kozmap Links
         </Link>
         <Link
-          href='/?tab=oracleLinks'
-          className={`w-full text-center underline-offset-4 underline transition-all delay-100 ${currentTab === 'oracleLinks' ? 'decoration-neutral-50 font-bold' : 'decoration-neutral-500 font-medium hover:decoration-neutral-200'}`}
+          href='https://'
+          target='_blank'
+          className='w-full text-center underline-offset-4 underline transition-all delay-100 decoration-neutral-500 font-medium hover:decoration-neutral-200'
         >
           Oracle Links
         </Link>
       </div>
 
-      <AnimatePresence mode='wait'>
-        <motion.div
-          key={currentTab}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
-          className='w-full min-h-full flex flex-col gap-2 pt-4'
-        >
-          {tabs[currentTab].map((link) => (
-            <Link
-              href={link.href}
-              key={link.id}
-              className='w-full bg-neutral-900 p-2 md:p-4 rounded-md hover:shadow-lg hover:shadow-neutral-900 transition-all delay-100'
-            >
-              {link.name}
-            </Link>
-          ))}
-        </motion.div>
-      </AnimatePresence>
+      <div className='w-full min-h-full flex flex-col gap-2 pt-4'>
+        {kozmapLinks.map((link) => (
+          <Link
+            href={link.href}
+            key={link.id}
+            className='w-full bg-neutral-900 p-2 md:p-4 rounded-md hover:shadow-lg hover:shadow-neutral-900 transition-all delay-100'
+          >
+            {link.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,43 +1,13 @@
-'use client';
-import { useEffect, useRef, useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useSearchParams } from 'next/navigation';
-
 export const Video = () => {
-  const searchParams = useSearchParams();
-  const activeTab = searchParams.get('tab');
-  const [currentTab, setCurrentTab] = useState('');
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-
-  useEffect(() => {
-    if (activeTab === 'kozmapLinks') {
-      setCurrentTab('/kozmapLinks');
-    } else {
-      setCurrentTab('/oracleLinks');
-    }
-  }, [activeTab]);
-
   return (
-    <AnimatePresence mode='popLayout'>
-      <motion.video
-        key={currentTab}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.15 }}
-        ref={videoRef}
-        preload='auto'
-        autoPlay
-        loop
-        className='rounded-3xl size-full max-h-max md:max-w-sm max-w-[100vw]'
-      >
-        <track default kind='captions' srcLang='en' src='SUBTITLE_PATH' />
-        {currentTab === '/kozmapLinks' ? (
-          <source src='/kozm.mp4' type='video/mp4' />
-        ) : (
-          <source src='/oracle.mp4' type='video/mp4' />
-        )}
-      </motion.video>
-    </AnimatePresence>
+    <video
+      preload='auto'
+      autoPlay
+      loop
+      className='rounded-3xl size-full max-h-max md:max-w-sm max-w-[100vw]'
+    >
+      <track default kind='captions' srcLang='en' src='SUBTITLE_PATH' />
+      <source src='/kozm.mp4' type='video/mp4' />
+    </video>
   );
 };
